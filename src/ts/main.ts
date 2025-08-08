70a4a73488907f13e3f8f55b30956d4a4e018700
Rick Gray <rickdgray@outlook.com>
2025-08-07 23:02:35 -0500

    setup carousel

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,21 @@
-const navigation = document.querySelector('.navbar') as HTMLElement | null;
+import EmblaCarousel from 'embla-carousel'
+import Autoplay from 'embla-carousel-autoplay';
 
+// grab elements
+const navigation = document.querySelector('.navbar') as HTMLElement;
+const emblaNode = document.querySelector('.embla') as HTMLElement;
+
+// nav autoscroll
 const navigationHeight = navigation ? navigation.offsetHeight + 20 : 0;
-
 document.documentElement.style.setProperty('--scroll-padding', `${navigationHeight}px`);
+
+// carousels for rescues
+const options = {
+    loop: true
+};
+
+const plugins = [Autoplay()];
+
+const emblaApi = EmblaCarousel(emblaNode, options, plugins);
+
+console.log(emblaApi.slideNodes());
